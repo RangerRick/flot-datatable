145f9c06ac8414095dcb44f262d8269c0954912b
Jesse White <jesse@opennms.org>
2015-10-30 11:07:50 -0400

    Add a test to verify the generated table html.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
+
+function createTable(allSeries, xformat, yformat) {
+    var T = '<table><tr><th>'+(allSeries[0].xaxis.options.axisLabel ? allSeries[0].xaxis.options.axisLabel : 'x')+'</th>', t = '';  // if no x - label name: x
+    for (var j = 0; j < allSeries.length; j++) {
+        T += '<th>'+(allSeries[j].yaxis.options.axisLabel ? allSeries[j].yaxis.options.axisLabel : 'y'+j)+'</th>';   // create 1st header row, if no y - label name: yN
+    }
+    T += '</tr>';
+    for (var i = 0; i < allSeries[0].data.length; i++) {                  // for each x
+        t='<tr><th>'+xformat(allSeries[0].data[i][0])+'</th>'    // 1st colunm, x-value
+        for (var j = 0; j < allSeries.length; j++) {         // for each series
+            t += '<td>'+yformat(allSeries[j].data[i][1])+'</td>' // add y-data
+        };
+        t += '</tr>';
+        T += t;
+    };
+    T += '</table>';
+    return T
+}
+
 function init(plot) {
 
-    plot.makeTableHolder = function(){
+    plot.makeTableHolder = function() {
         var placeholder = plot.getPlaceholder();
         var tabplace = $('<div class="tabplace" style="width: '+placeholder[0].clientWidth+'px;"><div class="tab" id="graphTab">Graph</div><div class="tab" id="dataTab">Data</div></div>');
-        var T = $('<div title="Doubleclick to copy" class="tdata" style="width: '+placeholder[0].clientWidth+'px; height: '+placeholder[0].clientHeight+'px; padding: 0px; position: relative; overflow: scroll; background: white; z-index: 10; display: none;">'+makeTable()+'</div>');
+        var T = $('<div title="Doubleclick to copy" class="tdata" style="width: '+placeholder[0].clientWidth+'px; height: '+placeholder[0].clientHeight+'px; padding: 0px; position: relative; overflow: scroll; background: white; z-index: 10; display: none;">' +
+            createTable(plot.getData(), plot.getOptions().datatable.format.x, plot.getOptions().datatable.format.y) +
+            '</div>');
 
         $(placeholder)
             .wrap("<div class='wrapper'></div>")
@@ -11,28 +32,8 @@ function init(plot) {
             .append(T);
         bindTabs(tabplace, T);
         bindTable(T);
-    }
+    };
 
-    function makeTable(){
-        var D = plot.getData();
-     // if D.length > 0            ----  catch errors
-        var ftt = d3.format(".4f");
-        var T = '<table><tr><th>'+(D[0].xaxis.options.axisLabel ? D[0].xaxis.options.axisLabel : 'x')+'</th>', t = '';  // if no x - label name: x
-        for (var j = 0; j < D.length; j++) {
-            T += '<th>'+(D[j].yaxis.options.axisLabel ? D[j].yaxis.options.axisLabel : 'y'+j)+'</th>';   // create 1st header row, if no y - label name: yN
-        }
-        T += '</tr>';
-        for (var i = 0; i < D[0].data.length; i++) {                  // for each x
-            t='<tr><th>'+ftt(D[0].data[i][0])+'</th>'    // 1st colunm, x-value
-            for (var j = 0; j < D.length; j++) {         // for each series
-                t += '<td>'+ftt(D[j].data[i][1])+'</td>' // add y-data
-            };
-            t += '</tr>';
-            T += t;
-        };
-        T += '</table>';
-        return T
-    }
     function bindTabs(tabs, table){
         tabs.click(function(e){
             switch (e.target.id) {
--- test/jquery.flot.datatable.test.js
+++ test/jquery.flot.datatable.test.js
@@ -2,7 +2,26 @@ describe('jquery.flot.datatable', function () {
 
     describe('.createTable', function() {
         it('should generate the appropriate html', function() {
+            var allSeries = [
+                {
+                    metric: 'xx',
+                    id: 'xx',
+                    xaxis: {
+                        options: {
 
+                        }
+                    },
+                    yaxis: {
+                        options: {
+
+                        }
+                    },
+                    data: [[0, 100], [1, 101]]
+                }
+            ];
+
+            var tableHtml = createTable(allSeries, d3.format(".2f"), d3.format(".2f"));
+            expect(tableHtml).toBe("<table><tr><th>x</th><th>y0</th></tr><tr><th>0.00</th><td>100.00</td></tr><tr><th>1.00</th><td>101.00</td></tr></table>");
         });
     });
 
